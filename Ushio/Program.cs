4087e38fb17f87081f0df24759d519e7d65e7748
Jonathan Markman <jmarkman446@gmail.com>
2020-12-11 01:47:54 -0500

    Add project files.

---
+++ Ushio/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Ushio
+{
+    class Program
+    {
+        static Task Main(string[] args) => new Startup(args).StartAsync();
+    }
+}
---
+++ Ushio/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ushio
+{
+    public class Startup
+    {
+        private IConfigurationRoot _config;
+
+        public Startup(string[] args)
+        {
+            var cfgBuilder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory, "Configuration"))
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+            _config = cfgBuilder.Build();
+        }
+
+        public async Task StartAsync()
+        {
+
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+
+        }
+    }
+}
